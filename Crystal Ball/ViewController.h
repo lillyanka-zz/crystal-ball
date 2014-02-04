e270672fa34623497d82911a259ab1f1b5c53950
Lilliana Víquez Murillo <sittingducks@gmail.com>
2014-02-04 10:51:15 -0600

    Initial Commit

---
+++ Crystal Ball/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Crystal Ball
+//
+//  Created by Lilliana Víquez Murillo on 2/4/14.
+//  Copyright (c) 2014 Lilliana Víquez Murillo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Crystal Ball/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Crystal Ball
+//
+//  Created by Lilliana Víquez Murillo on 2/4/14.
+//  Copyright (c) 2014 Lilliana Víquez Murillo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
